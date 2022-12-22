55695313e042ab7343edc409a2354617b415039d
Trusov <a.trusov@voskhod.ru>
2022-12-22 14:00:44 +0400

    Initial commit

---
+++ main/vars.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	var v1 int //v1 == 0
+	fmt.Println(v1)
+
+	var v2 int = 100
+	fmt.Println(v2)
+
+	v3 := 5
+	fmt.Println(v3)
+
+	v4 := 5
+	//v4 := 5
+	v4 = 10
+	fmt.Println(v4)
+
+	var v5, v6 = 1, 2
+	v5, v6 = 3, 4
+
+	var v7 int
+	v5, v6, v7 = 1, 2, 3
+	v5, v6 = v6, v5
+
+	var (
+		v01 = 1
+		v02 = "string"
+	)
+
+}
